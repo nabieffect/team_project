1a5171eb2d0888ca0c440e10012fe3c4268260b8
dkrwlq <dkrwlq@naver.com>
2017-08-24 15:03:09 +0900

    20170824 수정 최종본

---
+++ js_src/nb-3-2-2.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2017-07-21.
+ */
+
+$(function(){
+
+  $(window).on('scroll',function(){
+
+    var scrollValue=$(window).scrollTop();
+
+    if(scrollValue >=200){
+      $('.nb322-title-pic-fixed').addClass('display');
+    }
+
+    if(scrollValue<200){
+      $('.nb322-title-pic-fixed').removeClass('display');
+    }
+
+  });
+
+});
